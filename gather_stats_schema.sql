34317b0f54c004875548ed28a39c1230a72e7dbd
raymondcao18 <36083849+raymondcao18@users.noreply.github.com>
2018-04-23 09:19:50 -0700

    Update gather_stats_schema.sql

--- gather_stats_schema.sql
+++ gather_stats_schema.sql
@@ -1,7 +1,7 @@
 --This is used to collect schema stats
 --By raymond cao 4/12/2018
  exec dbms_stats.gather_schema_stats( -
-ownname          => 'PLANDATA', -
+ownname          => '&schema_name', -
 options          => 'GATHER AUTO', -
 estimate_percent => dbms_stats.auto_sample_size, -
 method_opt       => 'for all columns size auto', -
